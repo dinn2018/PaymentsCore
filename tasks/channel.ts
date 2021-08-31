bf410092bda3f2f2d9b55df731258a43d6f380a8
Lei Ding <dinn@duedex.com>
2021-08-31 14:16:51 +0800

    add permit function

--- tasks/channel.ts
+++ tasks/channel.ts
@@ -7,6 +7,9 @@ import { RootChannel } from './contracts'
 
 task('channel', 'params')
 	.addOptionalParam('child','child channel address')
+	.addOptionalParam('permit', 'permit contract to send msg to child channel')
+	.addOptionalParam('unpermit', 'unpermit contract to send msg to child channel')
+
 	.addOptionalParam('msg', 'receive message from child')
 	.setAction(async (args:any, env: HardhatRuntimeEnvironment)=>{
 		const channel = await RootChannel(env)
@@ -19,6 +22,18 @@ task('channel', 'params')
 		if (args.msg) {
 			const tx = await channel.receiveMessage(args.msg)
 			console.log('receiveMessage', tx)
+			const receipt = await tx.wait()
+			console.log('receipt', receipt)
+		}
+		if (args.permit) {
+			const tx = await channel.permit(args.permit, true)
+			console.log('permit', tx)
+			const receipt = await tx.wait()
+			console.log('receipt', receipt)
+		}
+		if (args.unpermit) {
+			const tx = await channel.permit(args.permit, false)
+			console.log('unpermit', tx)
 			const receipt = await tx.wait()
 			console.log('receipt', receipt)
 		}
