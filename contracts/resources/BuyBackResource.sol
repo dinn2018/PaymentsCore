12a4d4ef806756121c85e17458291a6594421541
Lei Ding <dinn@duedex.com>
2021-08-31 10:59:57 +0800

    new resource

---
+++ contracts/resources/BuyBackResource.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.3;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+import "../interfaces/IResource.sol";
+import "./ResourceWithChannel.sol";
+
+contract BuyBackResource is ResourceWithChannel, IResource {
+
+    using SafeMath for uint256;
+
+    using SafeERC20 for IERC20;
+
+    // valuation for resource.
+    address public override valuationToken;
+
+    address public override beneficiary;
+
+    uint256 public price;
+
+    IUniswapV2Router02 public routerV2;
+
+    address public buyBackReceiver;
+
+    mapping(address => uint256) public balances;
+
+    // For resource extral info
+    mapping(uint256 => bytes) public slots;
+
+    bytes4 public childBuy = bytes4(keccak256("buy(address,address,uint256)"));
+
+    constructor(
+        address owner,
+        IRootChannel _channel,
+        address _valuationToken,
+        address _buyBackReceiver,
+        IUniswapV2Router02 _routerV2,
+        uint256 _price
+    ) ResourceWithChannel(_channel) {
+        transferOwnership(owner);
+        valuationToken = _valuationToken;
+        buyBackReceiver = _buyBackReceiver;
+        routerV2 = _routerV2;
+        price = _price;
+        beneficiary = address(this);
+    }
+
+    function buy(address buyer, uint256 amount, uint256 value) external override onlyPermit {
+        balances[buyer] = balances[buyer].add(amount);
+        sendMessageToChild(abi.encodeWithSelector(childBuy, address(this), buyer, amount));
+        emit Bought(buyer, amount, value);
+    }
+
+    function spend(address buyer, uint256 amount) external override onlyChannel {
+        require(balances[buyer] >= amount, "Resource: not enough resources to spend.");
+        balances[buyer] = balances[buyer].sub(amount);
+        emit Spent(buyer, amount);
+    }
+
+    function setPrice(uint256 _price) external onlyOwner {
+        price = _price;
+    }
+
+    function setSlot(uint256 key, bytes memory value) external onlyOwner {
+        slots[key] = value;
+    }
+
+    function getValue(uint256 amount) external view override returns (uint256) {
+        return amount.mul(price);
+    }
+
+    function getAmount(uint256 value) external view override returns (uint256) {
+        return value.div(price);
+    }
+
+	function transferBuyBackReceiver(address newReceiver) external onlyOwner {
+		buyBackReceiver = newReceiver;
+	}
+
+	function buyBack(
+		address[] memory path,
+		uint256 value,
+		uint256 amountOutMin,
+		uint256 deadline
+	) external onlyOwner {
+		require(path.length > 1, "BuyBack: path invalid.");
+ 		IERC20(path[0]).safeApprove(address(routerV2), value);
+        routerV2.swapExactTokensForTokens(
+            value,
+            amountOutMin,
+            path,
+            buyBackReceiver,
+			deadline
+        );
+	}
+
+}
